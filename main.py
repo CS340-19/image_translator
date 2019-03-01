117b560c262b8637f7fb59817e3e6ff7cc8e0406
Dakota Williams <Dakobrah@gmail.com>
2019-03-01 09:51:59 -0500

    Added Error checking for language and input text
    Returning data correctly.
    Currently working on getting the upload button to work!

--- main.py
+++ main.py
@@ -13,13 +13,7 @@ class Application(tk.Frame):
 
   def createWidgets(self):
     self.translate = tk.Text(self, height=3, width=50)
-    self.translate.grid()
-
-    self.uploadButton = tk.Button(self, text='Upload', command=self.upload)
-    self.uploadButton.grid(padx=5, pady=5)
-
-    self.button = tk.Button(self, text="QUIT", command=self.quit)
-    self.button.grid(padx=5, pady=5)
+    self.translate.grid(column=1)
 
     self.choices = tk.Listbox(self)
     self.choices.insert(1, 'Chinese')
@@ -27,10 +21,32 @@ class Application(tk.Frame):
     self.choices.insert(3, 'Finnish')
     self.choices.insert(4, 'Japanese')
     self.choices.insert(5, 'Spanish')
-    self.choices.grid()
+    self.choices.grid(column=1)
+
+    #self.uploadButton = tk.Button(self, text='Browse', command=self.askopenfile)
+    #self.uploadButton.grid(row=1, column=2, padx=2, pady=2)
+
+    self.uploadButton = tk.Button(self, text='Upload', command=self.upload)
+    self.uploadButton.grid(row=2, column=2, padx=2, pady=2)
+
+    self.button = tk.Button(self, text="QUIT", command=self.quit)
+    self.button.grid(row=3, column=2, padx=2, pady=2)
+
+  def askopenfile(self):
+    return tk.filedialog.askopenfile(mode='r', **self.file_opt)
 
   def upload(self):
-    print("Testing upload button!")
+    try:
+        current_selection = self.choices.get(self.choices.curselection())
+        print(current_selection)
+    except:
+        print("No Language Selected!")
+
+    try:
+        current_text = self.translate.get("1.0", 'end')
+        print(current_text)
+    except:
+        print("No Text")
 
 app = Application()
 app.mainloop()
