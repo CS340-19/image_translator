8a120a291502f76abb8bd16dd6c0355c236d4a96
YimingSun60 <33106916+YimingSun60@users.noreply.github.com>
2019-03-25 01:02:30 +0800

    Detect text from image

---
+++ ocr.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import io
+from google.cloud import vision
+from google.cloud.vision import types
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+path = os.path.join(os.path.dirname(__file__),sys.argv[1])
+
+detect_text(path)
+
+
+
